51abec14f370cb01750a72c469027a8004fcee33
Shweta Kadam <shwetarkadam25@gmail.com>
2026-03-29 14:06:41 +0530

    Update clipd.rb

--- Formula/clipd.rb
+++ Formula/clipd.rb
@@ -2,7 +2,7 @@ class Clipd < Formula
   desc "Multi-slot clipboard manager with searchable history"
   homepage "https://clipd.sh"
   url "https://github.com/shwetarkadam/clipd/releases/download/v0.1.0-alpha/clipd-macos-v0.1.0-alpha.zip"
-  sha256 "609e221102b86b4b1c66efc76e..."  # paste the full SHA from the release page
+  sha256 "609e221102b86b4b1c66efc76e4dee3e9ca9272f4da16f7f7638aa20860dffc7"
   version "0.1.0-alpha"
   license "BUSL-1.1"
 
